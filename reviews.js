da66e22a123d38c55f6080cadb8082d86e9e4302
Chris Dawson <xrdawson@gmail.com>
2014-08-19 13:05:58 -0700

    Add second item

--- reviews.js
+++ reviews.js
@@ -1,4 +1,7 @@
 var YOUROOH_REVIEWS = [
     { title: 'Diaper Genie is awesome', 
-      description: 'Go ahead and just use that old trash can to store diapers. Watch as your friends begin to stop returning your calls. You\'ll notice the cats hanging out at friends\' houses more and more. The dog will pretend to be rabid so he can have a weekend in the pound. Using a trash can to keep disposable diapers stinks horribly. Don\'t do that. Instead, use a Diaper Genie', ASIN: '12345' }
+      description: 'Go ahead and just use that old trash can to store diapers. Watch as your friends begin to stop returning your calls. You\'ll notice the cats hanging out at friends\' houses more and more. The dog will pretend to be rabid so he can have a weekend in the pound. Using a trash can to keep disposable diapers stinks horribly. Don\'t do that. Instead, use a Diaper Genie', ASIN: '12345' },
+    { title: 'Don\'t bother with a fixed wheel running stroller',
+      description: 'So, we thought we were being smart purchasing an old fixed wheel Bob stroller at a garage sale. A good price. Saving money! But, the reality is we never use it. The fixed wheel strollers don\'t collapse well in the car, they don\'t manouver well at the store. If I had to do it all over again, I would have invested in a movable wheel stroller. It makes a difference.',
+      ASIN: '23123' }
 ];
