1c0005c3ef04a9f44f5801f83077a5a21f0457f5
Chris Dawson <xrdawson@gmail.com>
2014-08-19 12:02:55 -0700

    Switch to reviews.js

--- file.js
+++
@@ -1,3 +0,0 @@
-var YOUROOH_REVIEWS = [
-    { title: 'Diaper Genie is awesome', description: 'Go ahead and just use that old trash can to store diapers. Watch as your friends begin to stop returning your calls. You'll notice the cats hanging out at friends' houses more and more. The dog will pretend to be rabid so he can have a weekend in the pound. Using a trash can to keep disposable diapers stinks horribly. Don\'t do that. Instead, use a Diaper Genie', ASIN: '12345' }
-];
